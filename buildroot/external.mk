6d8c26936a3a81ba4f39aa4e0d3a8e813586ece1
jpvc36 <117010945+jpvc36@users.noreply.github.com>
2026-09-20 13:01:06 +0200

    Modify TAS5805M download site and options
    Updated TAS5805M_SITE to use GitHubusercontent for downloads and added extra download options.

--- buildroot/external.mk
+++ buildroot/external.mk
@@ -1,8 +1,13 @@
 TAS5805M_VERSION = main
-TAS5805M_SITE = https://github.com/jpvc36/TAS5805m/raw/refs/heads/main
+TAS5805M_SITE = https://githubusercontent.com
 TAS5805M_SOURCE = tas5805m.c
 TAS5805M_SITE_METHOD = wget
+
+# Tell Buildroot not to expect a compressed archive
+TAS5805M_EXTRA_DOWNLOAD_OPTS = 
+
 TAS5805M_DEPENDENCIES = alsa-utils
+TAS5805M_MODULE_MAKE_OPTS = CONFIG_SND_SOC_TAS5805M=m
 
 define TAS5805M_EXTRACT_CMDS
 	cp $(TAS5805M_DL_DIR)/$(TAS5805M_SOURCE) $(@D)/
@@ -10,3 +15,4 @@ define TAS5805M_EXTRACT_CMDS
 endef
 
 $(eval $(kernel-module))
+
